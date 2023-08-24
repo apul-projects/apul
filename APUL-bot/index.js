b156a98866e32a34617c294553db8b3571e55115
PhoenixH2010 <123717414+PhoenixH2011H@users.noreply.github.com>
2023-08-25 06:37:08 +0930

    Update index.js

--- APUL-bot/index.js
+++ APUL-bot/index.js
@@ -17,8 +17,50 @@ const Discord = require("discord.js");
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
 
 client.on("messageCreate", async message => {
- if(message.content === "!textcommand") {
-  await message.channel.send("Your text goes here!")
+ if(message.content === "!test") {
+  await message.channel.send("My servers are on and I havent discovered any bugs!")
+ 
+
+  if(message.content === "!website") {
+  await message.channel.send("https://projects.apul.pro/")
+  } 
+
+  if(message.content === "!add") {
+  await message.channel.send("Sorry, We dont support that yet, But please enjoy all my other features!")
+  }
+  
+  if(message.content === "@everyone") {
+  await message.channel.send("We don't care so stfu")
+  }
+  
+  if(message.content === "@here") {
+  await message.channel.send("We dont care so stfu")
+  }
+  
+  if(message.content === "@everyone") {
+  await message.channel.send("We dont care so stfu")
+  }
+  
+  if(message.content === "@phoenixh1467") {
+  await message.channel.send("Who said you could @ my daddy??")
+  }
+  
+  if(message.content === "@liafaith_2000") {
+  await message.channel.send("Who said you could @ my mommy??")
+  }
+  
+  if(message.content === "!fuck you") {
+  await message.channel.send("too!")
+  }
+  
+  if(message.content === "!nigger") {
+  await message.channel.send("Well, at least I aint a monkey..")
+  }
+  
+  if(message.content === "!nigga") {
+  await message.channel.send("Well, at least I aint a monkey..")
+  }
+
   } 
  })      
 
